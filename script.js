d9389f5abf788c8acde83157a003c6fc42e09722
Shady Coder <shadysaleh01@gmail.com>
2020-07-27 23:37:42 -0700

    finished html and css files

---
+++ script.js
@@ -0,0 +1,28 @@
+var wrapper = document.querySelector("#wrapper")
+var qDiv = document.querySelector("#qDiv")
+var startQuiz = document.querySelector("startQuiz")
+
+
+var questions = [
+   {
+      q: "How many stats are there in the USA?",
+      choices: ["49", "51", "38", "50"],
+      a: "50"
+   },
+   {
+      q: "In which city was the Titanic launched?",
+      choices: ["Whitehead", "Helen’s Bay", "Holywood", "Balfast"],
+      a: "Balfast"
+   },
+   {
+      q: "3. What does the white dove symbolize?",
+      choices: ["Love", "Peace", "Happiness", "Sorrow"],
+      a: "Peace"
+   },
+   {
+      q: "How many milligrams make a gram?",
+      choices: ["100", "1000", "1100", "10000"],
+      a: "1000"
+   },
+]
+
